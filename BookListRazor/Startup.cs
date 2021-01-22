52bc1f418f78b903d6639d53bd48d157de2a2e52
Ankit Panwar <shivankitgroup@gmail.com>
2021-01-22 13:30:33 +0530

    Web API and AJAX Call

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -26,7 +26,9 @@ namespace BookListRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +54,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
---
+++ BookListRazor/wwwroot/js/BookList.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();     
+});
+function loadDataTable() {
+    dataTable = $('#DT_Book').dataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns": [
+            { "data": "name", "widht": "20%"},
+            { "data": "author", "widht": "20%" },
+            { "data": "isbn", "widht": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                              <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer:width:70px;'>Edit</a>
+                            &nbsp;
+                            <a  class='btn btn-danger text-white' style='cursor:pointer:width:70px;'>Delete</a>
+                            <div>`;
+                },"widht":"20%"
+            }
+        ],
+        "language": {
+            "emptyTable":"No data found"
+        }
+    });
+}
